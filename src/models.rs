d07eee063c4cf05b54748545292339134d9daef5
LucsOtero <lucasamorimotero@gmail.com>
2026-06-04 20:51:52 -0300

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,17 +1,27 @@
 use serde::{Serialize, Deserialize};
+use sqlx::FromRow;
+use askama::Template;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, FromRow)]
 pub struct User {
     pub id: i32,
     pub email: String,
     pub password: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, FromRow, Clone)]
 pub struct Investment {
     pub id: i32,
-    pub user_id: i32,
+    pub user_id: Option<i32>,
     pub asset: String,
     pub quantity: f64,
     pub price: f64,
+}
+
+// Estrutura que conecta o Rust com o arquivo HTML
+#[derive(Template)]
+#[template(path = "dashboard.html")]
+pub struct DashboardTemplate {
+    pub total_value: f64,
+    pub investments: Vec<Investment>,
 }
